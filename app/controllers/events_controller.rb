ea0f39fff1533064d731a5d76627dcfc88cad2d3
Schifferking <schifferking@hotmail.com>
2023-04-22 15:00:49 -0600

    Add events actions and form
    Added logic to new and create actions. New just creates an empty event.
    Create makes a new event and receives params from the new private helper
    method params_helper. After, it uses the build_ method to create the
    association between an event and the creator (a user). Maybe this line
    will be changed later.
    
    The form just contains labels for the fields required and the proper
    tags to enter the proper values. The 'event_date' field uses a
    datetime_local_field to let the user select the day and hour of the
    event.
    
    Also added in the events controller a helper method from devise to ask
    the user to sign before creating an event.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,22 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(params_required)
+    @creator = @event.build_creator
+  end
+
   def index
     @events = Event.all
+  end
+
+  private
+
+  def params_required
+    params.require(:event).permit(:name, :event_date, :location)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "events#index"
 
-  resources :events, only: [:index]
+  resources :events, only: [:new, :create, :index]
   resources :users, only: [:show]
 end
